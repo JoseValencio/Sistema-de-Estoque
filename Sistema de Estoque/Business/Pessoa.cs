4bcbf8bbbf81172598e83cffda24d5d175a68001
jose valencio <valencio.santos@hotmail.com>
2022-07-22 18:53:46 -0300

    Projeto Oficina

---
+++ Sistema de Estoque/Business/Pessoa.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+
+namespace Business
+{
+     public class Pessoa
+    {
+
+       
+        //Comando Sql
+        ConexaoBanco conexao = new ConexaoBanco();
+        SqlCommand cmd = new SqlCommand();
+       public string message = " ";
+
+
+        //Construtor da classe.
+        public Pessoa(string nome, string cpf,  string dataCadastro, string dataNascimento,
+            string observacao )
+        {
+
+
+            cmd.CommandText = "insert into Pessoa(Nome, CPF, Observacao, Data_Cadastro, Data_Nascimento " +
+                    ") values (@nome,  @cpf, @Data_Nascimento, @Data_Cadastro, @observacao )";
+
+            //parametros
+            cmd.Parameters.AddWithValue("@nome", nome);
+          // cmd.Parameters.AddWithValue("@rg", rg);
+            cmd.Parameters.AddWithValue("@cpf", cpf);
+            cmd.Parameters.AddWithValue("@data_Nascimento", dataNascimento);
+            cmd.Parameters.AddWithValue("@data_cadastro", dataCadastro);
+            cmd.Parameters.AddWithValue("@observacao", observacao);
+            
+
+
+            //Conexão com o Banco
+
+            try
+            {
+                cmd.Connection = conexao.Conectar();
+
+                //executar comando de insert
+                cmd.ExecuteNonQuery();
+                // desconecta do banco 
+                conexao.Desconectar();
+
+                // mensgem de sucesso.
+
+                this.message = "Cadastro realizado!";
+            }
+            catch (SqlException erro )
+            {
+                this.message = "Erro ao conectar com o banco" + erro;
+            }
+
+       
+
+      
+
+
+        }
+
+     }
+}
+
