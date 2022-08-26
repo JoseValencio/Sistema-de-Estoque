148bfa8046ad465f927409522e6c2305282f5baa
jose valencio <valencio.santos@hotmail.com>
2022-08-26 00:30:45 -0300

    Projeto Pastelaria2 Usando entity Framework

--- Pastelaria2/Program.cs
+++ Pastelaria2/Program.cs
@@ -13,11 +13,25 @@ namespace Pastelaria2.Classes
         {
             using (var context = new Pastelaria2Dbcontext())
             {
-                context.Pasteis.Add(new Pastel() { Preco = 2.0, Sabor = "Carne", Tamanho = "Medio" });
-                context.Hamburgers.Add(new Hamburger() { Nome = "X-Tudo", Preco = 10.50, Tamanho = "Grande", Sabor = "Queijo" });
-                context.SaveChanges();
-            }
+                 context.Pasteis.Add(new Pastel() { Preco = 2.0, Sabor = "Carne", Tamanho = "Medio" });
+                 context.Hamburgers.Add(new Hamburger() { Nome = "X-Tudo", Preco = 10.50, Tamanho = "Grande", Sabor = "Queijo" });
+                 context.SaveChanges();
 
+               
+                foreach (var pastel in context.Pasteis)
+                {
+                    Console.WriteLine($"{pastel.Id} {pastel.Preco} {pastel.Sabor} {pastel.Tamanho}");
+                   
+                }
+                Console.Read();
+
+
+
+
+            }      
+          
+
+            
         }
     }
 }
